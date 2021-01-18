7f25816a164864168c274e6e30aa1fd268bbe128
RezeR1320 <artem.gromyko.00@gmail.com>
2021-01-18 18:56:46 +0300

    task1 completed

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const styles = {
+    Header: {
+        padding: "10px 20px",
+        textAlign: "center",
+        color: "blue",
+        fontSize: "30px",
+        fontWeight: "bold" 
+    },
+    Card: {
+        textAlign: "Center",
+        fontWeight: "normal"
+    }
+};
+
+function Card(props) {
+    return (
+        <div style={styles.Card}>
+            <h2 style={{color: "orange", fontSize: "35px"}}>Caption</h2>
+            <p>Hello world</p>            
+        </div>);
+}
+
+function Task(props) {
+    return (
+        <div>
+            <header style={styles.Header}>
+                <h1>This is the header</h1>
+            </header>
+            <Card/>
+        </div>
+    );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <Task/>,
+    document.getElementById('root')
+  );
