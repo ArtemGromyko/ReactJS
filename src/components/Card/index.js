e91a6c86f0776b66fd64a90088b3d930bd3beb67
RezeR1320 <artem.gromyko.00@gmail.com>
2021-01-22 05:56:46 +0300

    task2 completed

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import styles from './card.module.css';
 
-function Card(props) {
-    return (
-        <div className={styles.Card}>
-            <h2 className={styles.CardCaption}>Caption</h2>
-            <p>Hello world</p>
-        </div>
-    );
+class Card extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            cardStyle: 'CardUnChecked',
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange() {
+        this.setState((prevState) => {
+            return {
+                cardStyle:
+                    prevState.cardStyle === 'CardUnChecked'
+                        ? 'CardChecked'
+                        : 'CardUnChecked',
+            };
+        });
+    }
+
+    render() {
+        return (
+            <div className={styles[this.state.cardStyle]}>
+                <h2 className={styles.CardCaption}>Caption</h2>
+                <input type="checkbox" onChange={this.handleChange} />
+                <p>Hello world</p>
+            </div>
+        );
+    }
 }
 
 export default Card;
