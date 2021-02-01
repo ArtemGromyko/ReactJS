7fb67a553bf82681acb040309d7f62ec2febcec2
RezeR1320 <artem.gromyko.00@gmail.com>
2021-02-01 11:08:38 +0300

    task3 changed

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import CardDisplay from '../CardDisplay';
-import CardEdit from '../CardEdit';
+import Card from '../Card';
 import styles from './body.module.css';
 
 class Body extends React.Component {
@@ -8,23 +7,7 @@ class Body extends React.Component {
         super(props);
         this.state = {
             display: true,
-            caption: 'caption',
-            text: 'hello world',
         };
-
-        this.changeDisplayState = this.changeDisplayState.bind(this);
-        this.changeValue = this.changeValue.bind(this);
-    }
-
-    changeDisplayState() {
-        this.setState({ display: !this.state.display });
-    }
-
-    changeValue(captionValue, textValue) {
-        console.log(captionValue, textValue);
-        this.setState({ caption: captionValue });
-        this.setState({ text: textValue });
-        this.changeDisplayState();
     }
 
     render() {
@@ -33,35 +16,10 @@ class Body extends React.Component {
                 <header className={styles.BodyHeader}>
                     <h1>This is the header</h1>
                 </header>
-                {this.state.display ? (
-                    <CardDisplay
-                        clickCallBack={this.changeDisplayState}
-                        caption={this.state.caption}
-                        text={this.state.text}
-                    />
-                ) : (
-                    <CardEdit
-                        saveCallBack={this.changeValue}
-                        clickCallBack={this.changeDisplayState}
-                        caption={this.state.caption}
-                        text={this.state.text}
-                    />
-                )}
+                <Card />
             </div>
         );
     }
 }
 
-/*
-function Body(props) {
-    return (
-        <div>
-            <header className={styles.BodyHeader}>
-                <h1>This is the header</h1>
-            </header>
-            <Card />
-        </div>
-    );
-}
-*/
 export default Body;
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,18 +1,51 @@
 import React from 'react';
 import styles from './card.module.css';
 import classNames from 'classnames';
+import { BsPencilSquare } from 'react-icons/bs';
 
 class Card extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            captionValue: 'caption',
+            textValue: 'hello world',
+            currentCaptionValue: 'caption',
+            currentTextValue: 'hello world',
             checked: false,
+            display: true,
         };
-        this.handleChange = this.handleChange.bind(this);
+        this.StyleChangeHandler = this.StyleChangeHandler.bind(this);
+        this.DisplayStateChangeHandler = this.DisplayStateChangeHandler.bind(
+            this,
+        );
+        this.CaptionChangeHandler = this.CaptionChangeHandler.bind(this);
+        this.TextChangeHandler = this.TextChangeHandler.bind(this);
+        this.ChangeSaveHandler = this.ChangeSaveHandler.bind(this);
     }
 
-    handleChange() {
+    StyleChangeHandler() {
         this.setState({ checked: !this.state.checked });
+    }
+
+    DisplayStateChangeHandler() {
+        this.setState({ display: !this.state.display });
+        this.setState({ currentCaptionValue: this.state.captionValue });
+        this.setState({ currentTextValue: this.state.textValue });
+        this.setState({ checked: false });
+    }
+
+    CaptionChangeHandler(event) {
+        this.setState({ currentCaptionValue: event.target.value });
+    }
+
+    TextChangeHandler(event) {
+        this.setState({ currentTextValue: event.target.value });
+    }
+
+    ChangeSaveHandler() {
+        this.setState({ captionValue: this.state.currentCaptionValue });
+        this.setState({ textValue: this.state.currentTextValue });
+        this.DisplayStateChangeHandler();
     }
 
     render() {
@@ -26,9 +59,46 @@ class Card extends React.Component {
                         )
                     ]
                 }>
-                <h2 className={styles.CardCaption}>Caption</h2>
-                <input type="checkbox" onChange={this.handleChange} />
-                <p>Hello world</p>
+                {this.state.display ? (
+                    <>
+                        <input
+                            type="checkbox"
+                            onChange={this.StyleChangeHandler}
+                        />
+                        <button onClick={this.DisplayStateChangeHandler}>
+                            <BsPencilSquare />
+                        </button>
+                        <h2 className={styles.CardCaption}>
+                            {this.state.captionValue}
+                        </h2>
+                        <p>{this.state.textValue}</p>
+                    </>
+                ) : (
+                    <>
+                        <button onClick={this.ChangeSaveHandler}>Save</button>
+                        <button onClick={this.DisplayStateChangeHandler}>
+                            Discard
+                        </button>
+                        <br />
+                        <br />
+                        <input
+                            type="text"
+                            value={this.state.currentCaptionValue}
+                            onChange={this.CaptionChangeHandler}
+                            maxLength="10"
+                        />
+                        <br />
+                        <br />
+                        <input
+                            type="text"
+                            value={this.state.currentTextValue}
+                            onChange={this.TextChangeHandler}
+                            maxLength="50"
+                        />
+                        <br />
+                        <br />
+                    </>
+                )}
             </div>
         );
     }
